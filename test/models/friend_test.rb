b57b80ca17e68818e1e650f1ffe1e8eee977e6ac
Carl Hinderer <carl.hinderer4@gmail.com>
2017-04-14 23:55:59 -0600

    Added tests for Friend model validations.

--- test/models/friend_test.rb
+++ test/models/friend_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class FriendTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "requires first_name" do 
+  	friend = Friend.new(last_name: 'Doe', email: 'jdoe@gmail.com')
+  	assert_not friend.save, 'Saved friend without first name'
+  	assert_not_empty friend.errors[:first_name]
+  end
+
+  test "requires last_name" do
+  	friend = Friend.new(first_name: 'John', email: 'jdoe@gmail.com')
+  	assert_not friend.save, 'Saved friend without last name'
+  	assert_not_empty friend.errors[:last_name]
+  end
+
+  test "requires email" do
+  	friend = Friend.new(first_name: 'John', last_name: 'Doe')
+  	assert_not friend.save, 'Saved friend without email'
+  	assert_not_empty friend.errors[:email]
+  end
 end
