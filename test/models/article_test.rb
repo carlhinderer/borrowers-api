783be2eba847c09decc4b9a478b1d5dbab040ee3
Carl Hinderer <carl.hinderer4@gmail.com>
2017-04-15 23:46:58 -0600

    Added test for article model validations.

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'requires name'  do
+  	article = Article.new
+  	assert_not article.save, 'Saved article without name'
+  	assert_not_empty article.errors[:name]
+  end
 end
